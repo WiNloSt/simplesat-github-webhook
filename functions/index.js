e3ad18eae7a8f48e36117a40e8305f6d693fb516
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-05-04 00:29:48 +0700

    :sparkles: Validate Github request

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,27 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const crypto = require('crypto')
+const safeCompare = require('safe-compare')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const SECRET = functions.config().github.secret
+
+function IsPayloadFromGithub(request) {
+  const theirHash = request.get('X-Hub-Signature')
+  const ourHash =
+    'sha1=' +
+    crypto
+      .createHmac('sha1', SECRET)
+      .update(JSON.stringify(request.body))
+      .digest('hex')
+
+  const IshashEqual = safeCompare(ourHash, theirHash)
+
+  return IshashEqual
+}
+
+exports.webhook = functions.https.onRequest((request, response) => {
+  if (!IsPayloadFromGithub(request)) {
+    return response.status(403).send('Payload validation unsuccessful')
+  }
+
+  return response.send('OK!')
+})
